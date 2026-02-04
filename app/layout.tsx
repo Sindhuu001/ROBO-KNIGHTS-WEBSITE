c1bc710061887bbbec0619fecc1556a0225656c2
rohitlingarker <rohitlingerker@gmail.com>
2026-02-04 18:42:14 +0530

    testing vercel deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
+        {/* <Navbar /> */}
         <div className="pt-30">
         {children}
         </div>
