93fd53964ec8d943dbefc3e971bbc0c95adecf9d
rohitlingarker <rohitlingerker@gmail.com>
2026-02-04 15:04:37 +0530

    added margin on top

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
+        <div className="mt-30">
         {children}
+        </div>
         <Footer />
       </body>
     </html>
